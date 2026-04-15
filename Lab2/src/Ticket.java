798424cebabd08d1e7b4668854078ab3c563e70c
KoiPro <115386123+KoiPro@users.noreply.github.com>
2026-04-15 21:31:04 +0700

    Ticket.java

--- Lab2/src/Ticket.java
+++ Lab2/src/Ticket.java
@@ -1,5 +1,14 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Ticket{
+    int priority;
+    String createAt;
+    String ticketID;
+    public Ticket(int priority, String createAt, String ticketID){
+        this.priority = priority;
+        this.createAt = createAt;
+        this.ticketID = ticketID;
+    }
+    @Override
+    public String toString(){
+        return "priority" + priority + ",createAt" + createAt + ",ticketID" + ticketID;
     }
 }
